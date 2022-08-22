10b796a52e6d36378c10a8abd6ef06f750e5498e
Yaqub Tawab Olamilekan <61840059+Marktech2002@users.noreply.github.com>
2022-08-22 23:52:56 +0100

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -7,14 +7,15 @@ using namespace std;
 
 
 int main ()
-{/*
+{
     int name = 1;
      for(int i = 1 ; i <= 10; i++) {
+         // vibes 
             for(int j = 1; j < i; j++) {
              cout << "*";
             }
      cout << "*"<< endl;
-     }*/
+     }
 
      int n = 1 ;
      while(n<=50) {
